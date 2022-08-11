a5796893e70450c2d5bf409556f9d2b28fb420f7
Triyambaka Mishra <55938092+triyam@users.noreply.github.com>
2022-08-12 00:18:11 +0530

    dfwef

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 // import '../../Assets/css/footer.css';
-import Logo3 from '../../../../Assets/img/main/logo-3.png';
+import Logo3 from '';
 
 const AppFooter = () => (
 	<>
---
+++ src/Components/Home/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = () => (
+    <>
+      Hello
+      
+    </>
+)
+
+export default Navbar
