964e3530b94f983faefeb644bdeae3b80d331c50
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2022-08-12 00:59:43 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EndAdminDashboard from "./Components/Dashboard/EndAdminDashboard/EndAdminDashboard";
-import Lorem from './Components/Home/Sample/Lorem';
+// import Lorem from './Components/Home/Sample/Lorem';
+// import Footer from './Components/Home/Footer/Footer'
+// import './Assets/css/style.css';
 
 function App() {
   return(
     <>
-    
-    {/* <EndAdminDashboard/> */}
-    <Lorem />
+    <EndAdminDashboard/>
+    {/* <Lorem /> */}
+    {/* <Footer/> */}
+    {/* hi */}
     </>
   );
 }
