32c17a71b4afc7883c6da6659567439f726b616f
Triyambaka Mishra <55938092+triyam@users.noreply.github.com>
2022-08-12 16:16:23 +0530

    3rafcwef

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,7 +4,6 @@ import GolfDashboard from "./Dashboard/GolfDashboard/GolfDashboard"
 import SiteSeeingAuthorityDashboard from "./Dashboard/SiteSeeingAuthorityDashboard/SiteSeeingAuthorityDashboard"
 import TravelDashboard from "./Dashboard/TravelDashboard/TravelDashboard"
 import UserDashboard from "./Dashboard/UserDashboard/UserDashboard"
-import LoginAdmin from "./Auth/Login/LoginAdmin";
 import RegisterAdmin from "./Auth/Register/RegisterAdmin";
 import Lorem from './Home/Sample/Lorem';
 import Lorem2 from './Home/Sample/Lorem2';
@@ -18,7 +17,6 @@ const Main = () => (
       {/* <SiteSeeingAuthorityDashboard /> */}
       {/* <TravelDashboard/> */}
       {/* <UserDashboard /> */}
-      {/* <LoginAdmin/> */}
       <RegisterAdmin/>
 
       <Footer />
